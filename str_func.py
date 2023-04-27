1aa332b6144440580b1894d500dc6c03364b4bf6
abykov <abykov@uma.tech>
2023-04-27 18:04:33 +0300

    add func first_letters_high

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def high_text(some_text):
+    """ Принимает на вход строку и возвращает ее со всеми заглавными буквами """
     return some_text.upper()
+
+
+def first_letters_high(string):
+    """ Делает заглавными первые буквы каждого слова в строке """
+    return string.title()
